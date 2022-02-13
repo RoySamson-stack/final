225c83911bc8f0397992c98d59328e01b3d1f70c
RoySamson-stack <rsam9009@gmail.com>
2022-02-13 16:27:25 +0300

    making the header and connection it to the main page

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+
+
+function Header(){
+  return(
+    <div className="main_header">
+      <h1>Hello world</h1>
+      <div>
+      <p>Projects</p>
+      <p>Contact</p>
+      </div>
+      
+    </div>
+  )
+}
+export default Header
