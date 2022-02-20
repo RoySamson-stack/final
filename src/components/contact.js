110387c3730e5e4010fbc66bb78d2088cbf477cf
RoySamson-stack <rsam9009@gmail.com>
2022-02-20 15:57:20 +0300

    making the contact fomr work

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,21 +1,35 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 export default function Contact(){
-  return(
-    <div className="contact">
-      <form className="contact-form">
-      <h1 className="contact-title">Contact</h1>
+  const form = useRef();
 
-        <input type="text" placeholder="Name" className="name-input"/>
-        <br />
-        <input type="email" placeholder="Email" className="email-input" />
-        <br />
-        <textarea placeholder="Message" className="messages-input"></textarea>
-        <br />
-        <button type="submit">Send</button>
-      </form>
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_gb84o07', form.current, 'user_XU4VHJ6kyKU8vQJxJzXdu')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      form.current.reset();
+  };
+
+  return (
+    <div className="contact">
+        <form ref={form} onSubmit={sendEmail} className="contact-form">
+      <label>Name</label>
+      <input type="text" name="name" className="name-input"/>
+      <label>Email</label>
+      <input type="email" name="email" className="email-input"/>
+      <label>Subject</label>
+      <input type="text" name="subject" className="subject-input"/>
+      <label>Message</label>
+      <textarea name="message" className="messages-input"/>
+      <input type="submit" value="Send" className="contact-btn"/>
+    </form>
     </div>
-  )
-}
+  
+  );
+};
