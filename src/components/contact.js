a1c2310c76d6e75c6350dfd1459a863c60221f9d
RoySamson-stack <rsam9009@gmail.com>
2022-02-20 20:34:49 +0300

    installing email js and making a contact form send me emails

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,35 +1,43 @@
-import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
-export default function Contact(){
+export default function Contact() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'template_gb84o07', form.current, 'user_XU4VHJ6kyKU8vQJxJzXdu')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_143xt2i",
+        "template_gb84o07",
+        form.current,
+        "user_XU4VHJ6kyKU8vQJxJzXdu"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      form.current.reset();
+        }
+      );
+    form.current.reset();
   };
 
   return (
     <div className="contact">
-        <form ref={form} onSubmit={sendEmail} className="contact-form">
-      <label>Name</label>
-      <input type="text" name="name" className="name-input"/>
-      <label>Email</label>
-      <input type="email" name="email" className="email-input"/>
-      <label>Subject</label>
-      <input type="text" name="subject" className="subject-input"/>
-      <label>Message</label>
-      <textarea name="message" className="messages-input"/>
-      <input type="submit" value="Send" className="contact-btn"/>
-    </form>
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
+        <label>Subject</label>
+        <input type="text" name="subject" className="subject-input" />
+        <label>Name</label>
+        <input type="text" name="name" className="name-input" />
+        <label>Email</label>
+        <input type="email" name="email" className="email-input" />
+        <label>Message</label>
+        <textarea name="message" className="messages-input" />
+        <input type="submit" value="Send" className="contact-btn" />
+      </form>
     </div>
-  
   );
-};
+}
