a7a557fbde67d7b0c530f08e4efb167a7c657965
RoySamson-stack <rsam9009@gmail.com>
2022-03-02 23:37:05 +0300

    changing the background color and adding a dark and liht theme mode for the website

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Typewriter from 'typewriter-effect';
+import $ from 'jquery' 
+
+class Header extends Component{
+
+  Jquerycode = () =>{
+    ('.switch').addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      });
+  }
+  
 
 
-function Header(){
-  return(
-    <div className="main_header">
-      <Typewriter
-      className="typewriter"
-      onInit={(typewriter) =>{
-          typewriter.typeString("<h1>Hello world</h1>")
-          .pauseFor(2000)
-          .deleteAll()
-          .typeString("<h1>Welcome to my page!</h1>")
-          .deleteAll()
-          .typeString('<h1>Thank you for visiting my page</h1>')
-          .start();
-        }}
-      /> 
-      <div className="header_btn">
-      <a href="#projects" className="project_btn" id="">Projects</a>
-      <a href="#contacts" className="contact_btn">Contact</a>  
-      </div>   
-    </div>
-  )
-}
+  componentDidMount(){
+    this.Jquerycode()
+  }
+
+  render(){
+    return(
+      <div className="main_header">
+        <Typewriter
+        className="typewriter"
+        onInit={(typewriter) =>{
+            typewriter.typeString("<h1>Hello world</h1>")
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString("<h1>Welcome to my page!</h1>")
+            .deleteAll()
+            .typeString('<h1>Thank you for visiting my page</h1>')
+            .start();
+          }}
+        /> 
+        <div className="header_btn">
+        <a href="#projects" className="project_btn" id="">Projects</a>
+        <a href="#contacts" className="contact_btn">Contact</a>  
+        <div class="switch">
+          <div class="flicker">
+            <div class="moon"></div>
+          </div>
+        </div>
+        </div>  
+       
+      </div>
+    )
+  }
+  }
+ 
 export default Header
--- src/components/main.js
+++ src/components/main.js
@@ -3,6 +3,7 @@ import React from "react";
 function Main() {
   return (
     <div className="main_body">
+    
       <p className="name">Roy Samson</p>
       <p className="role">I am a full stack developer</p>
       <div className="language_container">
@@ -11,15 +12,16 @@ function Main() {
         <p className="language cube" id="js"></p>
         <p className="language cube" id="react"></p>
         <p className="language cube" id="node"></p>
-        <p className="language cube" id="express">Express</p>
+        <p className="language cube" id="express">
+          Express
+        </p>
         <p className="language cube" id="mongo"></p>
         <p className="language cube" id="sql"></p>
         <p className="language cube" id="git"></p>
         <p className="language cube" id="github"></p>
         <p className="language cube" id="php"></p>
       </div>
-      
     </div>
-  )
+  );
 }
 export default Main;
