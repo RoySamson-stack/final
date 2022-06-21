192c27394953be0d3e9ba4a2e4aa80c436cfd0a6
RoySamson-stack <rsam9009@gmail.com>
2022-06-21 17:27:10 +0300

    making  loeader file for the preloder

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import Contact from "./components/contact"
 import About from "./components/about"
 import Project from "./components/projectFinal"
 import Main from "./components/main"
+// import Loader from "./components/loader"
 import "./App.scss"
 import "./index.css"
+
 import {useRef, useState} from "react"
 
 function App() {
-  const contact = useRef(null)
-  const projects = useRef(null)
-
-  const scrollToSection = (elementRef) => {
-    window.scrollTo({
-      top: elementRef.current.offsetTop,
-      behavior: 'smooth'
-    })
-  }
+  // const [loading, setLoading] = useState
 
   return (
     <div className="App">
+      {/* <Loader /> */}
       <Header />
       <Main />
       <About />
--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,9 @@ import React from "react";
 // import profile from "./img/me.JPG"
 import Github from "./img/github.png";
 import Linkedin from "./img/linkedin.png";
+import "./cube.css"
+
+
 
 
 function About() {
@@ -17,6 +20,15 @@ function About() {
         Currently am  taking part in a Software Engineering programs at <a className="text-accent" href="https://www.jengaschool.com/" target="blank">Jenga school</a> to increase my programming language pool and further my knowledge.
       </p>
       </div>
+      <div class="slider">
+      <div class='container-a'>
+        <div class='slide x'></div>
+        <div class='slide y'></div>
+        <div class='slide z'></div>
+      </div>
+	  <div class='shadow'></div>
+   </div>	
+
     </div>
     </div>
   )
